4fe55493ed76fc13bbe6efba120229669c8c5f5e
opilane <opilane@HP-CZC2349HT3>
2023-10-20 10:30:10 +0300

    added 3rd form

--- Kolmnurk2.cs
+++ Kolmnurk2.cs
@@ -10,7 +10,7 @@ using System.Windows.Forms;
 
 namespace Naidis_Form
 {
-    public partial class Kolmnurk : Form
+    public partial class Kolmnurk2 : Form
     {
 
         private TextBox txtPointA;
@@ -21,52 +21,59 @@ namespace Naidis_Form
         private Label lblAreaResult;
         private Label lblPerimeterResult;
         private ListBox lstTriangleInfo;
-        Button btn;
+        private Button btnClear;
 
-        public Kolmnurk()
+        PictureBox pb;
+
+
+
+
+        public Kolmnurk2()
         {
-            this.Text = "Kolmnurk";
-            this.Height = 500;
-            this.Width = 500;
-
+            this.Text = "Kolmnurk2";
+            this.Height = 700;
+            this.Width = 700;
             InitializeComponent();
             InitializeUI();
         }
-
-        private void btnClear_Click(object sender, EventArgs e)
-        {
-            Invalidate();
-            lstTriangleInfo.Items.Clear();
-        }
-
-
-
-
         private void InitializeUI()
         {
-     
+
+            this.BackColor = Color.FromArgb(54, 50, 49);
+
             txtPointA = new TextBox();
             txtPointA.PlaceholderText = "Sisesta a pool";
             txtPointA.Location = new Point(10, 10);
             txtPointA.Size = new Size(150, 25);
-            
+            txtPointA.BackColor = Color.FromArgb(54, 50, 49);
+            txtPointA.ForeColor= Color.White;
+
 
             txtPointB = new TextBox();
             txtPointB.PlaceholderText = "Sisesta b pool";
             txtPointB.Location = new Point(10, 40);
             txtPointB.Size = new Size(150, 25);
-       
+            txtPointB.BackColor = Color.FromArgb(54, 50, 49);
+            txtPointB.ForeColor = Color.White;
+
 
             txtPointC = new TextBox();
             txtPointC.PlaceholderText = "Sisesta c pool";
             txtPointC.Location = new Point(10, 70);
             txtPointC.Size = new Size(150, 25);
+            txtPointC.BackColor = Color.FromArgb(54, 50, 49);
+            txtPointC.ForeColor = Color.White;
+
+
 
             btnDrawTriangle = new Button();
             btnDrawTriangle.Text = "Joonista";
             btnDrawTriangle.Width = 100;
             btnDrawTriangle.Location = new Point(10, 100);
             btnDrawTriangle.Click += btnDrawTriangle_Click;
+            btnDrawTriangle.BackColor = Color.Pink;
+
+
 
             lblHeightResult = new Label();
             lblHeightResult.Text = "Kõrgus: ";
@@ -83,26 +90,40 @@ namespace Naidis_Form
             lstTriangleInfo = new ListBox();
             lstTriangleInfo.Location = new Point(10, 160);
             lstTriangleInfo.Size = new Size(300, 300);
+            lstTriangleInfo.BackColor= Color.FromArgb(54, 50, 49);
+            lstTriangleInfo.ForeColor = Color.White;
 
-            btn = new Button();
-            btn.Text = "Vajutage siia";
-            btn.Location = new Point(130,100);
-            btn.Click += Btn_Click;
+            btnClear = new Button();
+            btnClear.Text = "Selge";
+            btnClear.Width = 100;
+            btnClear.Location = new Point(btnDrawTriangle.Location.X+130, btnDrawTriangle.Top);
+            btnClear.Click += btnClear_Click;
+            btnClear.BackColor = Color.Pink;
 
 
-            Controls.Add(btn);
+
+            pb = new PictureBox();
+            pb.Location = new Point(350,300);
+   
+            pb.Size = new Size(200, 140);
+            pb.SizeMode = PictureBoxSizeMode.Zoom;
+            pb.BorderStyle = BorderStyle.Fixed3D;
+
+
+
+            Controls.Add(btnClear);
+            Controls.Add(pb);
             Controls.Add(lstTriangleInfo);
             Controls.Add(txtPointA);
             Controls.Add(txtPointB);
             Controls.Add(txtPointC);
             Controls.Add(btnDrawTriangle);
-
         }
 
-        private void Btn_Click(object? sender, EventArgs e)
+        private void btnClear_Click(object sender, EventArgs e)
         {
-            var myForm = new Kolmnurk2();
-            myForm.Show();
+            Invalidate();
+            lstTriangleInfo.Items.Clear();
         }
 
         private void btnDrawTriangle_Click(object sender, EventArgs e)
@@ -114,13 +135,16 @@ namespace Naidis_Form
                 // Проверяем, существует ли треугольник
                 if (pointA + pointB > pointC && pointA + pointC > pointB && pointB + pointC > pointA)
                 {
-                    // Расчет координат вершин треугольника
-                    double centerX = 500; 
-                    double centerY = 150; 
 
-                    double angleA = Math.PI / 2; 
+                    lstTriangleInfo.Items.Clear();
+
+                    // Расчет координат вершин треугольника
+                    double centerX = 500;
+                    double centerY = 150;
+
+                    double angleA = Math.PI / 2;
                     double angleB = angleA + Math.Acos((pointA * pointA + pointC * pointC - pointB * pointB) / (2 * pointA * pointC));
-                    double angleC = 3 * Math.PI / 2; 
+                    double angleC = 3 * Math.PI / 2;
 
                     double xA = centerX + pointA * Math.Cos(angleA);
                     double yA = centerY - pointA * Math.Sin(angleA);
@@ -133,10 +157,12 @@ namespace Naidis_Form
 
                     // Рисуем треугольник на форме
                     Graphics graphics = CreateGraphics();
-                    Pen pen = new Pen(Color.Black);
+                    Pen pen = new Pen(Color.White);
                     graphics.DrawLine(pen, (float)xA, (float)yA, (float)xB, (float)yB);
                     graphics.DrawLine(pen, (float)xB, (float)yB, (float)xC, (float)yC);
                     graphics.DrawLine(pen, (float)xC, (float)yC, (float)xA, (float)yA);
+
+                    
 
 
                     Triangle triangle = new Triangle(pointA, pointB, pointC);
@@ -151,8 +177,24 @@ namespace Naidis_Form
                     lstTriangleInfo.Items.Add($"Kõrgus: {triangle.OutputH()}");
                     lstTriangleInfo.Items.Add($"Olemas?: {triangle.ExistTriangle}");
                     lstTriangleInfo.Items.Add($"Võrdkülne: {triangle.IsEquilateral()}");
+                    if (triangle.IsEquilateral() == true)
+                    {
+                        pb.Image = new Bitmap("../../../stor.png");
+
+                    }
+
                     lstTriangleInfo.Items.Add($"Võrdhaarne: {triangle.IsIsosceles()}");
+                    if (triangle.IsIsosceles()==true)
+                    {
+                        pb.Image = new Bitmap("../../../ravno.png");
+
+                    }
                     lstTriangleInfo.Items.Add($"Erikülne: {triangle.IsScalene()}");
+                    if (triangle.IsScalene() == true)
+                    {
+                        pb.Image = new Bitmap("../../../eri.png");
+
+                    }
                 }
                 else
                 {
@@ -162,9 +204,9 @@ namespace Naidis_Form
             else
             {
                 MessageBox.Show("Kirjuta kolmnurga külgede korrektsed väärtused.");
-            }   
+            }
         }
-        
+
 
     }
 }
---
+++ Kolmnurk_2.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Naidis_Form
+{
+    public class Kolmnurk_2
+    {
+
+        public class Triangle
+        {
+            public double a;
+            public double b;
+            public double c;
+            public double h;
+            public double m;
+
+            public Triangle(double A, double B, double C)
+            {
+                a = A;
+                b = B;
+                c = C;
+
+            }
+
+            public bool IsEquilateral()
+            {
+                return a == b && a == c;
+            }
+
+            public bool IsIsosceles()
+            {
+                return a == b || a == c || b == c;
+                
+             
+
+            }
+
+            public bool IsScalene()
+            {
+                return a != b && a != c && b != c;
+            }
+
+
+            public string outputA()
+            {
+                return Convert.ToString(a);
+            }
+            public string outputB()
+            {
+                return Convert.ToString(b);
+            }
+            public string outputC()
+            {
+                return Convert.ToString(c);
+            }
+            public double Perimeter()
+            {
+                double p = 0;
+                p = a + b + c;
+                return p;
+            }
+            public double OutputH()
+            {
+
+                double s = Surface();
+                double h = 2 * s / a;
+                Debug.WriteLine("a: " + a);
+                Debug.WriteLine("s: " + s);
+                Debug.WriteLine("h: " + h);
+                return h;
+
+                ///double p2 = 0;
+                //p2 = (a + b + c)/2;
+
+                //double h = 0;
+                //h = (2 / a) * (Math.Sqrt(p2*(p2-a) * (p2-b) * (p2-c)));
+                //return h;
+            }
+
+            public double OutputMC()
+            {
+                double m = 0;
+
+                m = (1 / 2) * Math.Sqrt(2 * Math.Pow(a, 2)) + (2 * Math.Pow(b, 2)) - Math.Pow(c, 2);
+                return m;
+            }
+            public double OutputMA()
+            {
+                double m = 0;
+
+                m = (1 / 2) * Math.Sqrt(2 * Math.Pow(c, 2)) + (2 * Math.Pow(b, 2)) - Math.Pow(a, 2);
+                return m;
+            }
+            public double OutputMB()
+            {
+                double m = 0;
+
+                m = (1 / 2) * Math.Sqrt(2 * Math.Pow(a, 2)) + (2 * Math.Pow(c, 2)) - Math.Pow(b, 2);
+                return m;
+            }
+
+
+
+            public double Surface()
+            {
+                double s = 0;
+                double p = 0;
+                p = (a + b + c) / 2;
+                s = Math.Sqrt((p * (p - a) * (p - b) * (p - c)));
+                return s;
+            }
+
+            public double GetSetA
+            {
+                get { return a; }
+                set { a = value; }
+
+            }
+
+            public double GetSetB
+            {
+                get { return b; }
+                set { b = value; }
+            }
+            public double GetSetC
+            {
+                get { return c; }
+                set { c = value; }
+            }
+
+            public bool ExistTriangle
+            {
+                get
+                {
+
+                    if ((a < b + c) && (b < a + c) && (c < a + b))
+                        return true;
+                    else return false;
+
+
+
+
+                }
+            }
+
+
+        }
+    }
+}
